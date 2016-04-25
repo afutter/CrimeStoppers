61a4f72b333fba7eef20d2cc4a3d9f044af1b872
akiva <akiva.futter@gmail.com>
2016-04-25 17:51:13 -0400

    init

---
+++ app/src/main/java/com/example/akiva/crimestoppers/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.akiva.crimestoppers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
