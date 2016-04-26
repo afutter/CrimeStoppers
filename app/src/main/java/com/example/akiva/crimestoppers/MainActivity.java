a81eb99886bbe60a0feb5fc7183b81b22a30d37d
akiva <akiva.futter@gmail.com>
2016-04-25 23:40:02 -0400

    created new main page for map, moved old main page into settings page, adding a button to move between them

--- app/src/main/java/com/example/akiva/crimestoppers/MainActivity.java
+++ app/src/main/java/com/example/akiva/crimestoppers/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.akiva.crimestoppers;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private android.support.design.widget.FloatingActionButton mSettings;
+    private static final int SETTINGS = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println ("goodbye? world");
+
+        mSettings =(android.support.design.widget.FloatingActionButton)  findViewById(R.id.settingsButton);
+
+        mSettings.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
     }
 }
---
+++ app/src/main/java/com/example/akiva/crimestoppers/SettingsActivity.java
@@ -0,0 +1,20 @@
+package com.example.akiva.crimestoppers;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Akiva on 4/25/16.
+ */
+public class SettingsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        if(getIntent()!=null){
+
+        }
+
+    }
+}
