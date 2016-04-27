d67d71215001e01719da5ee20efd4440c0a731e5
feelemyer <pfeelemyer@gmail.com>
2016-04-27 01:13:08 -0400

    added XML parser methods to class to get the nearest crimes of a given point

--- app/src/main/java/com/example/akiva/crimestoppers/Crime.java
+++ app/src/main/java/com/example/akiva/crimestoppers/Crime.java
@@ -1,14 +1,27 @@
+package com.example.akiva.crimestoppers;
 
-public class Crime {
+public class Crime implements Comparable<Crime> {
 	String offense;
 	String address;
 	double xCoordinate;
 	double yCoordinate;
+	double distance;
 	
 	Crime (String offense, String address, double xCoordinate, double yCoordinate) {
 		this.offense = offense;
 		this.address = address;
 		this.xCoordinate = xCoordinate;
 		this.yCoordinate = yCoordinate;
+		this.distance = 0;
 	}
+
+	public int compareTo(Crime c2) {
+		if (distance < c2.distance)
+			return -1;
+		else if (distance > c2.distance)
+			return 1;
+		else
+			return 0;
+	}
+
 }
--- app/src/main/java/com/example/akiva/crimestoppers/Parser.java
+++ app/src/main/java/com/example/akiva/crimestoppers/Parser.java
@@ -7,29 +7,21 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.TreeSet;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-//com.example.akiva.crimestoppers;
-
 public class Parser {
 
+	Hashtable<String, Integer> crime_count = new Hashtable<String, Integer>(100);
+	LinkedList<Crime> data = new LinkedList<Crime>();
 
-	public static void main(String[] args) {
-
-		Hashtable<String, Integer> crime_count = new Hashtable<String, Integer>(100);
-		//LinkedList<Crime> data = new LinkedList();
-
-		try { //used for debugging, comment out if you want to pass in file
-
-			
-		} catch (/*FileNotFoundException*/ Exception e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
-		}
+	public void parse(InputStream in) {
 
 		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder dBuilder = null;
@@ -42,12 +34,13 @@ public class Parser {
 		Document doc = null;
 
 		try {
-			doc = dBuilder.parse(System.in);
+			doc = dBuilder.parse(in);
 		} catch (SAXException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (NullPointerException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -60,7 +53,7 @@ public class Parser {
 			if (nextCrime.getNodeType() == Node.ELEMENT_NODE) {
 				NodeList n2 = nextCrime.getChildNodes();
 
-				//Crime newCrime = null;
+				Crime newCrime;
 				String offense = null;
 				String address = null;
 				double xCoordinate = 0;
@@ -90,15 +83,13 @@ public class Parser {
 					}
 				}
 
-
-
-				//newCrime = new Crime(offense, address, xCoordinate, yCoordinate);
+				newCrime = new Crime(offense, address, xCoordinate, yCoordinate);
 				if(crime_count.get(offense) == null) {
 					crime_count.put(offense, 1);
 				} else {
 					crime_count.put(offense, crime_count.get(offense) + 1);
 				}
-				//data.add(newCrime);
+				data.add(newCrime);
 			}
 		}
 		for(String key: crime_count.keySet()) {
@@ -106,4 +97,53 @@ public class Parser {
 		}
 	}
 
+	private TreeSet<Crime> nearestCrimes(double lat, double lon) {
+
+		TreeSet<Crime> tree = new TreeSet<Crime>();
+		for (Crime c : data) {
+			c.distance = distance(lat, lon, c.xCoordinate, c.yCoordinate, "N");
+			tree.add(c);
+		}
+		return tree;
+
+	}
+
+	private static double distance(double lat1, double lon1, double lat2, double lon2, String unit) {
+		double theta = lon1 - lon2;
+		double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+		dist = Math.acos(dist);
+		dist = rad2deg(dist);
+		dist = dist * 60 * 1.1515;
+		if (unit == "K") {
+			dist = dist * 1.609344;
+		} else if (unit == "N") {
+			dist = dist * 0.8684;
+		}
+
+		return (dist);
+	}
+	/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+	/*::	This function converts decimal degrees to radians						 :*/
+	/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+	private static double deg2rad(double deg) {
+		return (deg * Math.PI / 180.0);
+	}
+
+	/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+	/*::	This function converts radians to decimal degrees						 :*/
+	/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+	private static double rad2deg(double rad) {
+		return (rad * 180 / Math.PI);
+	}
+
+
+	public void run() {
+		/*Parser p = new Parser();
+		try {
+			p.parse(getResources().getAssets().open("ASAP__from03_27_2016__to04_07_2016.xml", 0));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}*/
+
+	}
 }
