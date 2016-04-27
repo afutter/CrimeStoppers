fa3a816b294a1253a0f97586b1659a122494abbf
feelemyer <pfeelemyer@gmail.com>
2016-04-26 21:20:32 -0400

    Fixed xml issue

--- app/src/main/java/com/example/akiva/crimestoppers/Parser.java
+++ app/src/main/java/com/example/akiva/crimestoppers/Parser.java
@@ -1,32 +1,32 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+package com.example.akiva.crimestoppers;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 import java.io.IOException;
 import java.util.Hashtable;
-import java.util.LinkedList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
+//com.example.akiva.crimestoppers;
 
 public class Parser {
+
 
 	public static void main(String[] args) {
 
 		Hashtable<String, Integer> crime_count = new Hashtable<String, Integer>(100);
-		LinkedList<Crime> data = new LinkedList();
+		//LinkedList<Crime> data = new LinkedList();
 
 		try { //used for debugging, comment out if you want to pass in file
-			System.setIn(new FileInputStream("C:\\Users\\Vincent Alonso\\Desktop\\CMSC436\\ASAP__from03_27_2016__to04_07_2016.xml"));
+
 			
-		} catch (FileNotFoundException e2) {
+		} catch (/*FileNotFoundException*/ Exception e2) {
 			// TODO Auto-generated catch block
 			e2.printStackTrace();
 		}
@@ -60,7 +60,7 @@ public class Parser {
 			if (nextCrime.getNodeType() == Node.ELEMENT_NODE) {
 				NodeList n2 = nextCrime.getChildNodes();
 
-				Crime newCrime = null;
+				//Crime newCrime = null;
 				String offense = null;
 				String address = null;
 				double xCoordinate = 0;
@@ -92,13 +92,13 @@ public class Parser {
 
 
 
-				newCrime = new Crime(offense, address, xCoordinate, yCoordinate);
+				//newCrime = new Crime(offense, address, xCoordinate, yCoordinate);
 				if(crime_count.get(offense) == null) {
 					crime_count.put(offense, 1);
 				} else {
 					crime_count.put(offense, crime_count.get(offense) + 1);
 				}
-				data.add(newCrime);
+				//data.add(newCrime);
 			}
 		}
 		for(String key: crime_count.keySet()) {
