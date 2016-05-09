5c0e811d186ca344686f7fdc2a76a2b843551742
akiva <akiva.futter@gmail.com>
2016-05-09 02:33:09 -0400

    checking intersections and movements

--- app/src/main/java/com/example/akiva/crimestoppers/MainActivity.java
+++ app/src/main/java/com/example/akiva/crimestoppers/MainActivity.java
@@ -1,25 +1,37 @@
 package com.example.akiva.crimestoppers;
 
+import android.Manifest;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -39,7 +51,7 @@ import java.util.Locale;
 import java.util.TreeSet;
 
 public class MainActivity extends FragmentActivity
-        implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
+        implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private GoogleApiClient mGoogleApiClient;
     private GoogleMap mMap;
@@ -48,9 +60,9 @@ public class MainActivity extends FragmentActivity
     private Location mLastLocation;
     private double[] mBounds = {38.803149,-77.11974,38.995548,-76.909393};
     //lower left Lat, lower left long, upper right lat, upper right long
-    private LocationManager mLocationManager;
+
     private List<Geofence> mGeofenceList;
-    private android.support.design.widget.FloatingActionButton mSettings_button;
+    private FloatingActionButton mSettings_button;
 
     private TreeSet<Crime> mCrimesList;
     private TreeSet<Crime> mBadCrimesList = new TreeSet<Crime>();//for crimes with bad addresses
@@ -60,14 +72,17 @@ public class MainActivity extends FragmentActivity
 
     private final int CIRCLE_SIZE = 1000;
     private final int RED_COLOR = 0x10ff0000;//first 2 digits control the transparency of the color
-    private final int START_ZOOM = 13; //change initial zoom with this
+    private final int START_ZOOM = 12; //change initial zoom with this
     private final String DATA = "ASAP__from03_27_2016__to04_07_2016.xml";
     private final String TAG = "MainActivity";
     public  final static String NOTIFICATION = "notification";
     private final int SETTINGS_REQUEST = 1;
     private final int MAX_CRIMES = 100; //Chance plotted crimes with this
     private LatLng mWashington = new LatLng(38.8951100, -77.0363700);
-    private LatLng[] movement= new LatLng[5];
+
+
+    private LatLng[] movement= new LatLng[4];
+    private LocationManager mLocationManager;
 
 
 
@@ -78,24 +93,29 @@ public class MainActivity extends FragmentActivity
         setContentView(R.layout.activity_main);
 
 
-        Button demo= (Button) findViewById(R.id.demo);
+        Button demo = (Button) findViewById(R.id.demo);
+        //movement[0]=new LatLng(38.931469, -77.051621 ); //start-->zoo
+        movement[0]=new LatLng(38.95447, -77.047119); //rock creek
+        movement[1]=new LatLng(38.928030, -77.048810); //cross bridge
+        movement[2]=new LatLng(38.923718, -77.047093); //walter pierce park
+        movement[3]=new LatLng(38.921652, -77.047068); // bird caught your eye
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        if(null == (mLocationManager =  (LocationManager)getSystemService(Context.LOCATION_SERVICE))){
-            finish();
-        }
+        //if(null == (mLocationManager =  (LocationManager)getSystemService(Context.LOCATION_SERVICE))){
+        //  finish();
+        //}
 
-        mSettings_button =(android.support.design.widget.FloatingActionButton)  findViewById(R.id.settingsButton);
+        mSettings_button = (FloatingActionButton) findViewById(R.id.settingsButton);
 
         mSettings_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-                if(mSettings!=null) {
+                if (mSettings != null) {
                     intent.putExtra("checkboxes", mSettings);
                 }
                 intent.putExtra("radius", mRadius);
@@ -105,32 +125,156 @@ public class MainActivity extends FragmentActivity
         mGeocoder = new Geocoder(this);
         //This creates an instance of a GoogleAPI client
         if (mGoogleApiClient == null) {
+            // ATTENTION: This "addApi(AppIndex.API)"was auto-generated to implement the App Indexing API.
+            // See https://g.co/AppIndexing/AndroidStudio for more information.
             mGoogleApiClient = new GoogleApiClient.Builder(this)
                     .addConnectionCallbacks(this)
                     .addOnConnectionFailedListener(this)
                     .addApi(LocationServices.API)
-                    .build();
+                    .addApi(AppIndex.API).build();
         }
         demo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // 1) inject crime
 
+                if (mLocationManager == null) {
+                    turnOnLocationManager();
+                }
 
+                Crime c = new Crime("HOMICIDE", "1369 Connecticut Ave NW, Washington, DC 20036, USA", "n/a", "n/a", 38.909627, -77.043381);
+                mCrimesList.add(c);
+                //for (int i = 0; i < movement.length; i++) {
+
+                turnOnLocationManager();
+                    updateLocation(movement[0]);
+                    Log.i("Loop", "got to next i. i=" + 0);
+                    for (Crime crime : mCrimesList) {
+                        if (intersects(crime.Lat, crime.Long, movement[0].latitude, movement[0].longitude)) {
+                            //call push
+                            String tracker = SettingsActivity.checkTracking(crime.offense,
+                                    distance(crime.Lat, crime.Long, movement[0].latitude, movement[0].longitude, "M"));
+                            if (!tracker.equals("Not Tracking")) {
+                                sendPushNotification(tracker);
+
+                            }
+                        }
+                    }
+                Handler handlerOne = new Handler();
+                handlerOne.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.i(TAG, "Pausing for 4 seconds");
+                        updateLocation(movement[1]);
+                        Log.i("Loop", "got to next i. i=" + 1);
+                        for (Crime crime : mCrimesList) {
+                            if (intersects(crime.Lat, crime.Long, movement[1].latitude, movement[1].longitude)) {
+                                //call push
+                                String tracker = SettingsActivity.checkTracking(crime.offense,
+                                        distance(crime.Lat, crime.Long, movement[1].latitude, movement[1].longitude, "M"));
+                                if (!tracker.equals("Not Tracking")) {
+                                    sendPushNotification(tracker);
+
+                                }
+                            }
+                        }
+                        Handler handlerTwo = new Handler();
+                        handlerTwo.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                Log.i(TAG, "Pausing for 4 seconds");
+                                updateLocation(movement[2]);
+                                Log.i("Loop", "got to next i. i=" + 2);
+                                for (Crime crime : mCrimesList) {
+                                    if (intersects(crime.Lat, crime.Long, movement[2].latitude, movement[2].longitude)) {
+                                        //call push
+                                        String tracker = SettingsActivity.checkTracking(crime.offense,
+                                                distance(crime.Lat, crime.Long, movement[2].latitude, movement[2].longitude, "M"));
+                                        if (!tracker.equals("Not Tracking")) {
+                                            sendPushNotification(tracker);
+
+                                        }
+                                    }
+                                }
+                                Handler handlerThree = new Handler();
+                                handlerThree.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Log.i(TAG, "Pausing for 4 seconds");
+                                        updateLocation(movement[3]);
+                                        Log.i("Loop", "got to next i. i=" + 3);
+                                        for (Crime crime : mCrimesList) {
+                                            Log.i("Finally!!", "hit the scene");
+                                            if (intersects(crime.Lat, crime.Long, movement[3].latitude, movement[3].longitude)) {
+                                                Log.i("Finally!!", "intersects");
+                                                //call push
+                                                String tracker = SettingsActivity.checkTracking(crime.offense,
+                                                        distance(crime.Lat, crime.Long, movement[3].latitude, movement[3].longitude, "M"));
+                                                if (!tracker.equals("Not Tracking")) {
+                                                    sendPushNotification(tracker);
+
+                                                }
+                                            }
+                                        }
+                                    }
+                                }, 4000);
+                            }
+                        }, 4000);
+                    }
+                }, 4000);
+
+
+
+
+
+
+                //shutdownLocationManager();
             }
+
+            //shutdownLocationManager();
         });
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                   0);
+        }
 
     }
     protected void onStart() {
         mGoogleApiClient.connect();
         super.onStart();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.akiva.crimestoppers/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(mGoogleApiClient, viewAction);
     }
 
     protected void onStop() {
         mGoogleApiClient.disconnect();
         super.onStop();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.akiva.crimestoppers/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(mGoogleApiClient, viewAction);
     }
     //Test Geofence
     public void TestGeofence(){
@@ -390,5 +534,46 @@ public class MainActivity extends FragmentActivity
         mNotifyMgr.notify(mNotificationId, mBuilder.build());
 
     }
+
+    private void turnOnLocationManager(){
+        mLocationManager=(LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        mLocationManager.addTestProvider(LocationManager.GPS_PROVIDER, false, false, false,
+                false, true, true, true, 0, 5);
+        mLocationManager.setTestProviderEnabled(LocationManager.GPS_PROVIDER, true);
+        //mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mLocationSource);
+    }
+    public void updateLocation(LatLng location){
+        if ( Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return  ;
+        }
+
+        mMap.setMyLocationEnabled(true);
+        Location mockLocation= new Location(LocationManager.GPS_PROVIDER);
+        mockLocation.setLatitude(location.latitude);
+        mockLocation.setLongitude(location.longitude);
+        mockLocation.setAltitude(0);
+        mockLocation.setTime(System.currentTimeMillis());
+        mockLocation
+                .setElapsedRealtimeNanos(SystemClock.elapsedRealtimeNanos());
+        mockLocation.setAccuracy(5);
+        mLocationManager.setTestProviderLocation(LocationManager.GPS_PROVIDER, mockLocation);
+        Log.i("LocationUpdate", "should have been updated");
+        //mMap.setLocationSource(new MyLocationSource(mockLocation));
+
+
+    }
+    private void shutdownLocationManager() {
+        mLocationManager.removeTestProvider(LocationManager.GPS_PROVIDER);
+    }
+    public boolean intersects(double Lat1, double Long1, double Lat2, double Long2){
+        if(distance(Lat1, Long2, Lat2, Long2, "M") <= mRadius){
+            return true;
+        }else{
+            return true;
+        }
+    }
+
 
 }
